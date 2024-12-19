0ed5c00675ca37fbf338d9bfbaa80391871220f9
devmilad <fazelianmilad3@gmail.com>
2024-12-19 20:03:01 +0330

    category api

--- src/components/Admin/AddCategory.jsx
+++ src/components/Admin/AddCategory.jsx
@@ -1,59 +1,56 @@
 import React, { useState } from "react";
+import  axiosClient  from "../../axios-client";
 
 const AddCategory = () => {
-  const [newProduct, setNewProduct] = useState({
-  
-    category: "",
-    image: null,
-  });
+  const [newProduct, setNewProduct] = useState(null);
 
-  const handleChange = (e) => {
-    const { name, value } = e.target;
-    setNewProduct((prev) => ({ ...prev, [name]: value }));
-  };
+	const handleImageChange = (e) => {
+		const file = e.target.files[0];
+		if (file) {
+			const reader = new FileReader();
 
-  const handleImageUpload = (e) => {
-    setNewProduct((prev) => ({ ...prev, image: e.target.files[0] }));
-  };
+			reader.onloadend = () => {
+				setNewProduct({ ...newProduct, image_url: reader.result });
+			};
 
-  const handleSubmit = () => {
-    console.log("New Product:", newProduct);
-   
+			reader.readAsDataURL(file); // base64
+		}
+	};
+
+  const handleSubmit = async(e) => {
+    e.preventDefault()
+    try {
+      const response = await axiosClient.post('/category', newProduct)
+      console.log(response.data)
+    } catch (error) {
+      console.log(error)
+    }
+
   };
 
   return (
-    <div>
+    <form encType="multipart/form-data">
+      <input
+        type="text"
+        name="name"
+        onChange={(e)=>setNewProduct(prev => ({...prev, name: e.target.value}))}
+        className="block w-full p-2 mb-2 border"
+        placeholder="نام دسته بندی"
+      />
       <input
         type="file"
-        name="image"
-        onChange={handleImageUpload}
+        name="image_url"
+        onChange={handleImageChange}
         className="block w-full p-2 mb-2 border"
       />
-   
-   
 
-      
-      <select
-        name="category"
-        value={newProduct.category}
-        onChange={handleChange}
-        className="block w-full p-2 mb-2 border"
-      >
-        <option value="steering">فرمان</option>
-        <option value="suspension">جلوبندی</option>
-        <option value="engine">موتور</option>
-        <option value="electrical">برقی</option>
-        <option value="standard">مکانیزم و استاندارد</option>
-        <option value="decoration">تزئینی</option>
-        <option value="gearbox">گیریبکس</option>
-      </select>
       <button
         onClick={handleSubmit}
         className="bg-green-500 text-white px-4 py-2 rounded"
       >
         افزودن دسته بندی
       </button>
-    </div>
+    </form>
   );
 };
 
